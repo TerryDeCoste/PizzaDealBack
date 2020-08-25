d9ea41b2a57967a0fced517c82d9acf650270fde
Terry DeCoste <tdecoste@otcauto.com>
2020-08-25 16:27:57 -0400

    Update Environment Variable

--- sql/database.js
+++ sql/database.js
@@ -1,7 +1,7 @@
 require('dotenv').config({path: __dirname + '/../process.env'}) //load process.env variables
 const { Pool } = require('pg');
 const pool = new Pool({
-    connectionString: process.env.PGCONNECTIONSTRING,
+    connectionString: process.env.DATABASE_URL,
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000,
 });
